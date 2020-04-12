ce596c9c4c6dd5a7490f4c9ab526e723feb5158c
Dustin Lau <dlau@comsoftdev.com>
2020-04-12 13:41:01 -0500

    added template builder components

--- lb.generator.handlebars.js
+++ lb.generator.handlebars.js
@@ -5,11 +5,24 @@ const { TemplateLoader } = require("./lib/TemplateLoader");
 const { TemplateResult } = require("./lib/TemplateResult");
 const { TemplateSettings } = require("./lib/TemplateSettings");
 
+const { FileHelper } = require("./lib/FileHelper");
+const { FileInformation } = require("./lib/FileInformation");
+const { RegexHelper } = require("./lib/RegexHelper");
+const { Replacement } = require("./lib/Replacement");
+const { Replacements } = require("./lib/Replacements");
+const { TemplateBuilder } = require("./lib/TemplateBuilder");
+
 module.exports = {
     HandlebarsHelpers,
     Helpers,
     Template,
     TemplateLoader,
     TemplateResult,
-    TemplateSettings
+    TemplateSettings,
+    FileHelper,
+    FileInformation,
+    RegexHelper,
+    Replacement,
+    Replacements,
+    TemplateBuilder
 };
